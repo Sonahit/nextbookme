4978aa8513cccd8c4da908251e52147d72142f49
Ivan Sadykov <grandpajok@gmail.com>
2020-01-01 07:07:06 +0600

    fix hocs

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "@layouts/Layout";
 
 import "@styles/about.scss";
+import withLogin from "../hocs/withLogin";
 
 const About = () => <Layout>About page</Layout>;
 
